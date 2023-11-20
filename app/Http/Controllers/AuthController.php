4a0707e655f383d5abe846f5ecae21a225dce97c
camiso2 <camiso2@gmail.com>
2023-11-20 12:12:36 -0500

    test credentials

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -51,6 +51,8 @@ class AuthController extends Controller
 
         } catch (JWTException $e) {
 
+            echo "test credentials";
+
             Log::info("Error exception login :: could_not_create_token./" . $e->getMessage());
             return response()->json(StatusController::eMessageError([$e->getMessage()], 'Error exception login.'));
 
@@ -211,19 +213,4 @@ class AuthController extends Controller
         }
     }
 
-   /* public function getAuthenticatedUser()
-    {
-        try {
-            if (!$user = JWTAuth::parseToken()->authenticate()) {
-                return response()->json(['user_not_found'], 404);
-            }
-        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-            return response()->json(['token_expired'], $e->getStatusCode());
-        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-            return response()->json(['token_invalid'], $e->getStatusCode());
-        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
-            return response()->json(['token_absent'], $e->getStatusCode());
-        }
-        return response()->json(compact('user'));
-    }*/
 }
